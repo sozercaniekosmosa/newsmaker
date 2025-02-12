a1fadfca95f60b46db9f32e90476441bcbff30bf
cherezov.vn <sozercaniekosmosa@gmail.com>
2025-02-12 23:29:57 +0500

    upd

--- src/tst/tst.js
+++ src/tst/tst.js
@@ -2,8 +2,6 @@ import ffmpeg from 'fluent-ffmpeg'
 import libffmpeg from '@ffmpeg-installer/ffmpeg'
 import libffprobe from '@ffprobe-installer/ffprobe'
 
-console.log(ffmpeg.path)
-
 ffmpeg.setFfmpegPath(libffmpeg.path);
 ffmpeg.setFfprobePath(libffprobe.path);
 
@@ -12,25 +10,26 @@ ffmpeg.setFfprobePath(libffprobe.path);
 
 // const command = ffmpeg();
 
-ffmpeg('input.png')
-    // .inputOptions('-loop 1') // Для создания видео из изображения
-    // .videoFilter(
-    // "zoompan=z='min(zoom-0.1,0.5)':d=1:x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)':s=1920x1080"
-    // "zoompan=z='min(max(zoom,pzoom)+0.0015,1.5)':x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)':d=1"
-    // -c:v libx264 -crf 18 -preset veryslow output.mp4
-    // )
+ffmpeg('D:\\Dev\\JS\\Prj\\2024\\newsmaker\\src\\tst\\input.png')
+    .inputOption([
+        "-vsync 0",
+        "-hwaccel nvdec",
+        "-hwaccel cuvid",
+        "-hwaccel_device 0"
+    ])
+    .videoFilter(
+        "scale=7680x4320:force_original_aspect_ratio=decrease,zoompan=z='min(zoom+.2/(30*5),1.2)':x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)':d=30*5:fps=30"
+    )
     // .outputOptions('-t 5') // Длительность видео
     // .videoCodec('libx264')
-    .outputOptions([
-        "-vf",
-        // "zoompan=z='zoom+0.0001':d=250:x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)'"
-        // "zoompan=z='min(zoom+0.001,2)':x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)'"
-        // "zoompan=z='min(max(zoom,pzoom)+0.0015,1.5)':d=120:x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)':fps=60:iw=981:ih=964:ow=981:oh=964"
-        // "scale=981:964,zoompan=z='zoom+0.002':d=120:x='if(gt(iw,ih),iw/2-(iw/zoom/2),(ow-iw)/2)':y='if(gt(iw,ih),(oh-ih)/2,ih/2-(ih/zoom/2))':s=1920x1080'"
-        "zoompan=z='min(zoom+0.0015,1.5)':x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)':d=250:s=981x964,scale=1920:1080"
-    ])
-    .output('output.mp4')
+    // .outputOptions([
+    //     "-vf",
+    //     "scale=7680x4320,zoompan=z='min(zoom+.2/125,1.2)':x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)':d=125"
+    // ])
+    .output('D:\\Dev\\JS\\Prj\\2024\\newsmaker\\src\\tst\\output.mp4')
     .on('start', (command) => console.log('Запущена команда: ' + command))
+    // .on('progress', ({frames}) => console.log(frames / 150 * 100))
+    .on('progress', ({frames}) => console.log(frames / 150 * 100))
     .on('end', () => console.log('Обработка завершена'))
     .on('error', (err) => console.error('Ошибка:', err))
     .run();
